cd6eb391a166df79eefda456fbfa9fd5396d417d
bom_d_van <bom.d.van@gmail.com>
2013-03-08 09:58:07 +0800

    print message that is more understandable

--- train.go
+++ train.go
@@ -10,14 +10,14 @@ func Run() {
 
     var server func(w http.ResponseWriter, r *http.Request)
 	if Config.BundleAssets {
-        fmt.Println("Mode[Production]")
+        fmt.Println("[Production]Serving assets from ./public/assets:\n")
 		server = servePublicAssets
 	} else {
-        fmt.Println("Mode[Development]")
+        fmt.Println("[Development]Serving assets from ./assets:\n")
 		server = serveAssets
 	}
     
-	// TODO: support using custom ServeMux
+	// TODO: support custom ServeMux
     http.Handle(Config.AssetsUrl, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         server(w, r)
     }))
