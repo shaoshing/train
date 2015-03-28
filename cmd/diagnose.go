bab27b911230f91ad958811046b5a5a3c21372e2
Jason Lee <huacnlee@gmail.com>
2015-03-28 20:02:22 +0800

    Update diagnose command to install npms

--- cmd/diagnose.go
+++ cmd/diagnose.go
@@ -5,7 +5,6 @@ import (
 	"github.com/huacnlee/train/interpreter"
 	"path"
 	"runtime"
-	"strings"
 )
 
 func diagnose() bool {
@@ -13,50 +12,34 @@ func diagnose() bool {
 
 	fmt.Println("== Diagnosing\n")
 
-	var rubyVersion string
-	rubyVersion, err = bash(`ruby -e "puts RUBY_VERSION"`)
-	if err != nil {
-		fmt.Println("-- SASS and CoffeeScript are disabled because ruby is not installed.")
-		fmt.Println("   (visit https://github.com/sstephenson/rbenv/#installation for installation instructions)")
-		return false
-	}
-
-	if !strings.Contains(rubyVersion, "2.2") {
-		fmt.Printf("-- Train requires Ruby version to be 1.9.x; you have %s", rubyVersion)
-		fmt.Println("   (Please install required Ruby version if you wish to use SASS or CoffeeScript)")
-		return false
-	}
-
 	allGood := true
 	_, filename, _, _ := runtime.Caller(1)
 	assetsPath := path.Dir(filename) + "/assets"
 
-	_, err = bash("gem which sass")
+	_, err = bash("npm install -g node-sass")
 	if err != nil {
-		fmt.Println("-- SASS is disabled because the required gem is not found.")
-		fmt.Println("   (install it if you wish to use SASS: gem install sass)\n")
+		fmt.Println("-- SASS is disabled because the required npm is not found.")
+		fmt.Println("   (install it if you wish to use SASS: npm install -g node-sass)\n")
 		allGood = false
 	} else {
 		_, err = interpreter.Compile(assetsPath + "/stylesheets/font.sass")
 		if err != nil {
 			fmt.Println("-- SASS is disabled because error raised while compiling. Error:")
 			fmt.Printf("%s\n", err.Error())
-			fmt.Println("(this might related to your Ruby Environment; try re-installing Ruby)\n")
 			allGood = false
 		}
 	}
 
-	_, err = bash("gem which coffee-script")
+	_, err = bash("npm install -g coffee-script")
 	if err != nil {
-		fmt.Println("-- CoffeeScript is disabled because the required gem is not found.")
-		fmt.Println("   (install it if you wish to use CoffeeScript : gem install coffee-script)")
+		fmt.Println("-- CoffeeScript is disabled because the required npm is not found.")
+		fmt.Println("   (install it if you wish to use CoffeeScript : npm install -g coffee-script)")
 		allGood = false
 	} else {
 		_, err = interpreter.Compile(assetsPath + "/javascripts/app.coffee")
 		if err != nil {
 			fmt.Println("-- CoffeeScript is disabled because error raised while compiling. Error: ")
 			fmt.Printf("%s\n", err.Error())
-			fmt.Println("(this might related to your Ruby Environment; try re-installing Ruby)\n")
 			allGood = false
 		}
 	}
